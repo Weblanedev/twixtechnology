0f5475ecf65503e696828d5aec1ca5a98426edad
Olu-martins <mayowasamuel@gmail.com>
2024-12-12 04:29:07 +0100

    updated footer

--- src/footer.tsx
+++ src/footer.tsx
@@ -5,16 +5,17 @@ const Footer = () => {
     <div className="w-full bg-blue-800 flex flex-col p-10 md:p-20 gap-10 md:px-[130px]">
       <div className="flex flex-col md:flex-row w-full items-center md:justify-between gap-5">
         <h3 className="text-white font-medium text-center md:text-left text-[22px] leading-[35.2px] md:w-[850px]">
-          Our highly skilled development teams specialized in Java, PHP, React,
-          Angular and AWS help you accelarate your business via modern custom
-          software solutions.
+          <strong className="text-[45px]">Got a Project?</strong> <br /> As
+          design strategists, we bridge the gap between strategy conceived and
+          strategy executed. We align the goals of your organisation to the
+          needs of your customers — creating exceptional brand experiences.
         </h3>
 
         <Link
-          to="/products"
+          to="/contact-us"
           className="text-base font-medium text-[#333] bg-white rounded-full px-7 md:px-12 py-4 md:py-5 hover:bg-gray-200 flex items-center justify-center gap-3 max-w-max"
         >
-          Explore Products
+          Ready to get started →
         </Link>
       </div>
 
@@ -23,32 +24,32 @@ const Footer = () => {
           to="/"
           className="text-[14px] leading-[24px] font-medium text-white cursor-pointer"
         >
-          Home
+          Go Home
         </Link>
         <Link
           to="/about-us"
           className="text-[14px] leading-[24px] font-medium text-white cursor-pointer"
         >
-          About
-        </Link>
-        <Link
-          to="/products"
-          className="text-[14px] leading-[24px] font-medium text-white cursor-pointer"
-        >
-          Products
-        </Link>
-        <Link
-          to="/contact-us"
-          className="text-[14px] leading-[24px] font-medium text-white cursor-pointer"
-        >
-          Contact
+          About Twix
         </Link>
         <Link
           to="/"
           className="text-[14px] leading-[24px] font-medium text-white cursor-pointer"
         >
-          Terms & Privacy
+          Explore Service
         </Link>
+        {/* <Link
+          to="/contact-us"
+          className="text-[14px] leading-[24px] font-medium text-white cursor-pointer"
+        >
+          Contact
+        </Link> */}
+        {/* <Link
+          to="/"
+          className="text-[14px] leading-[24px] font-medium text-white cursor-pointer"
+        >
+          Terms & Privacy
+        </Link> */}
       </div>
 
       <p className="text-[12px] capitalize leading-[24px] text-center text-gray-400">
